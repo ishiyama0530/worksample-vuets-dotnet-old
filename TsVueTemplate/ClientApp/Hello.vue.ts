1d3fc073c3c2a400622b13fd1274f3b12c4ac212
ishiyama <ishiyama0530@gmail.com>
2018-05-13 18:27:45 +0900

    cleaned.

--- TsVueTemplate/ClientApp/Hello.vue.ts
+++ TsVueTemplate/ClientApp/Hello.vue.ts
@@ -3,7 +3,6 @@ import { Component, Prop } from 'vue-property-decorator'
 
 @Component
 export default class App extends Vue {
-    // Makes a "exampleProperty" a component prop with the default value of 'Example'
     @Prop({ default: 'Hello Ts Vue Template.' })
     exampleProperty: string
 }
